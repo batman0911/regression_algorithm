cdc2bedafd5c7e5112edd9e5fcb7171e0ec973a1
Linh Nguyen <linhnm>
2022-08-16 11:04:41 +0700

    accelerated

--- kc_house/main.py
+++ kc_house/main.py
@@ -1,23 +1,53 @@
-
-class Greeting:
-    def __init__(self, name):
-        self.name = name
-
-    def say_hello(self):
-        print(f'hello dai ca {self.name}')
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import regression as reg
+import matplotlib.pyplot as plt
 
 
-class Tmp:
-    def __init__(self, name):
-        self.name = name
+def load_data():
+    df = pd.read_csv('../data/input/kc_house_data_cleaned.csv')
 
-    def show_name(self):
-        print(f'hello dai ca {self.name}')
+    X = df[df.columns[2:3]].to_numpy()
+    one = np.ones((X.shape[0], 1))
+    X = np.concatenate((one, X), axis=1)
+    y = df['price'].to_numpy()
+    # y = (y - np.min(y)) / (np.max(y) - np.min(y))
+    y = y / 1e6
+    y = y.reshape((X.shape[0], 1))
+
+    return train_test_split(X, y, test_size=0.2, random_state=42)
 
 
 if __name__ == '__main__':
-    tmp = Greeting("Linh Nguyen")
-    tmp.say_hello()
+    X_train, X_test, y_train, y_test = load_data()
+    w_init = np.repeat(1, X_train.shape[1]).reshape((X_train.shape[1], 1))
 
-    t = Tmp("Linh Nguyen")
-    t.show_name()
+    # %%
+    lm = reg.RegressionOpt(
+        max_iter=100,
+        w=w_init,
+        X_train=X_train,
+        y_train=y_train,
+        X_test=X_test,
+        y_test=y_test,
+    )
+
+    lm.fit_newton()
+
+    loss_func_list = lm.loss_func_list
+    grad_norm_list = lm.grad_norm_list
+
+    plt.plot(range(len(loss_func_list)), loss_func_list)
+    plt.title(f'Loss function', fontsize=16)
+    plt.ylabel('Value', fontsize=16)
+    plt.xlabel('Count', fontsize=16)
+    plt.show()
+
+    plt.plot(range(len(grad_norm_list)), grad_norm_list)
+    plt.title(f'Gradient norm', fontsize=16)
+    plt.ylabel('Value', fontsize=16)
+    plt.xlabel('Count', fontsize=16)
+    plt.show()
+
+    print(f'count: {lm.count}, gradient norm: {grad_norm_list[-1]}')
--- kc_house/regression.py
+++ kc_house/regression.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def cost_func(w, X, y):
+def loss_func(w, X, y):
     eps = y - np.dot(X, w)
     return float(np.dot(eps.T, eps) / 2)
 
@@ -26,30 +26,20 @@ def predict(X, w):
     return np.dot(X, w)
 
 
-class Tmp:
-    def __init__(self, name):
-        self.name = name
-
-    def show_name(self):
-        print(f'hello dai ca {self.name}')
-
-
 class RegressionOpt:
     def __init__(self,
-                solver='gd',
-                tol=1e-4,
-                max_iter=10000,
-                step_size=1,
-                check_after=1,
-                w=None,
-                X_train=None,
-                y_train=None,
-                X_test=None,
-                y_test=None,
-                lost_func=None,
-                grad_func=None,
-                bench_mode=False,
-                terminate=False):
+                 solver='gd',
+                 tol=1e-4,
+                 max_iter=10000,
+                 step_size=1,
+                 check_after=1,
+                 w=None,
+                 X_train=None,
+                 y_train=None,
+                 X_test=None,
+                 y_test=None,
+                 bench_mode=False,
+                 terminate=False):
         self.tol = tol
         self.max_iter = max_iter
         self.step_size = step_size
@@ -61,8 +51,6 @@ class RegressionOpt:
         self.y_train = y_train
         self.X_test = X_test
         self.y_test = y_test
-        self.loss_func = lost_func
-        self.grad_func = grad_func
         self.w = w
         self.count = 0
         self.bench_mode = bench_mode
@@ -72,12 +60,49 @@ class RegressionOpt:
         t = -self.step_size / self.X_train.shape[0]
         while self.count < self.max_iter:
             self.count += 1
-            grad = self.grad_func(self.w, self.X_train, self.y_train)
+            grad = gradient(self.w, self.X_train, self.y_train)
             self.w = update(self.w, t, grad)
             grn = np.linalg.norm(grad)
             if not self.bench_mode:
                 self.grad_norm_list.append(grn)
-                self.loss_func_list.append(self.loss_func(self.w, self.X_train, self.y_train))
+                self.loss_func_list.append(loss_func(self.w, self.X_train, self.y_train))
             if not self.terminate:
                 if grn < self.tol:
                     break
+        return self.w
+
+    def fit_newton(self):
+        t = self.step_size
+        while self.count < self.max_iter:
+            self.count += 1
+            grad = gradient(self.w, self.X_train, self.y_train)
+            H = hessian(self.X_train)
+            p = cal_direction(H, grad)
+            self.w = update(self.w, -t, p)
+            self.loss_func_list.append(loss_func(self.w, self.X_train, self.y_train))
+            grn = np.linalg.norm(grad)
+            self.grad_norm_list.append(grn)
+            if grn < self.tol:
+                break
+        return self.w
+
+    def fit_acc_gd(self):
+        t = self.step_size / self.X_train.shape[0]
+        w1 = update(self.w, t, gradient(self.w, self.X_train, self.y_train))
+
+        w = [self.w, w1]
+
+        self.count += 1
+        while self.count < self.max_iter:
+            self.count += 1
+            v = w[1] + (self.count - 2) / (self.count + 1) * (w[1] - w[0])
+            # v = w[1]
+            w[0] = w[1]
+            grad = gradient(v, self.X_train, self.y_train)
+            w[1] = update(v, t, -grad)
+            self.w = w[1]
+            self.loss_func_list.append(loss_func(w[1], self.X_train, self.y_train))
+            grn = np.linalg.norm(grad)
+            self.grad_norm_list.append(grn)
+            if grn < self.tol:
+                break
